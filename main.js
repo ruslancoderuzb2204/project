6ad1e58ae3e227a89e51074918cd6e438ef71e74
Muzaffar096 <muzaffar2131@gmail.comgit config --global user.emailmuzaffar2131@gmail.com>
2024-01-06 18:48:28 +0500

    rdstgrdt

--- main.js
+++ main.js
@@ -1,23 +1,24 @@
-const BASE_URL = 
+
+
+
 function checkUser() {
-    const loginData = JSON.parse(localStorage.getItem("loginData"));
-    if (!loginData) {
-      window.location.href = "login.html";
-    }
+  const loginData = JSON.parse(localStorage.getItem("loginData"));
+  if (!loginData) {
+    window.location.href = "login.html";
   }
-  checkUser();
-  
-  function logoutUser() {
-    const checkConfirm = confirm("Are you sure you want to log out?");
-    if (checkConfirm) {
-      localStorage.removeItem("loginData");
-      checkUser();
-    }
+}
+checkUser();
+
+function logoutUser() {
+  const checkConfirm = confirm("Are you sure you want to log out?");
+  if (checkConfirm) {
+    localStorage.removeItem("loginData");
+    checkUser();
   }
-  document.getElementById("logout-btn").addEventListener("click", logoutUser);
-  
-  const item = document.getElementById("item")
-  item.addEventListener("click",()=>{
-    window.location.href = "about.html"
-    
-  })
+}
+document.getElementById("logout-btn").addEventListener("click", logoutUser);
+
+const item = document.getElementById("item");
+item.addEventListener("click", () => {
+  window.location.href = "about.html";
+});
