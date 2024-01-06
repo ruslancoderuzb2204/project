fc3f64c8eaffa92212306390c227195da0f90290
znasriddinov <znasriddinov617@gmail.com>
2024-01-06 18:53:28 +0500

    asd

--- about.js
+++ about.js
@@ -5,4 +5,5 @@ let div = document.getElementById("div2");
 btn.addEventListener("click", () => {
   div.style.display = "none";
   card.style.display = "block";
+  card.style.margin = "50px auto 0 auto";
 });
